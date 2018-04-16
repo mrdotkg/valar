c5edad2d923c41086431593d446bb5fb94261cdc
Kumar Gaurav <grv.rkg@gmail.com>
2018-04-16 21:38:40 +0530

    bluscript components added

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import {Button, ButtonGroup, Intent, Spinner, AnchorButton} from "@blueprintjs/core";
+import {Button, ButtonGroup, Intent, Classes, Popover} from "@blueprintjs/core";
+import { FocusStyleManager } from "@blueprintjs/core";
+
+FocusStyleManager.onlyShowFocusOnTabs();
 
 let styles = require('./Home.scss');
-
-// using JSX:
-const mySpinner = <Spinner intent={Intent.PRIMARY} />;
 
 // use React.createElement if you're not using JSX.
 const myButton = React.createElement(Button, { intent: Intent.SUCCESS }, "Button content");
@@ -14,17 +14,24 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-        {mySpinner}
         <div className={styles.container} data-tid="container">
           <h2>Home</h2>
           <Link to="/counter"><Button intent={Intent.PRIMARY}>Counter</Button></Link>
           {myButton}
         </div>
-	      <ButtonGroup minimal={true} large={false}>
-		      <Button icon="database">Queries</Button>
-		      <Button icon="function">Functions</Button>
-		      <AnchorButton rightIcon="caret-down">Options</AnchorButton>
+
+	      <ButtonGroup className={Classes.ALIGN_LEFT}>
+		      <Popover >
+			      <Button icon="document" rightIcon="caret-down" text="File" />
+		      </Popover>
+		      <Popover >
+			      <Button icon="edit" rightIcon="caret-down" text="Edit" />
+		      </Popover>
+		      <Popover >
+			      <Button icon="eye-open" rightIcon="caret-down" text="View" />
+		      </Popover>
 	      </ButtonGroup>
+
       </div>
     );
   }
